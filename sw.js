9d06e616bfdd038df3747795a54eb95d38c6658f
costantinusa <costantinusa@gmail.com>
2026-08-19 20:56:45 +0200

    Añadir service worker PWA

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='rmc-gestion-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
