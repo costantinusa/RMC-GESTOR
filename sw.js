b46f4f30947b9797280e683fcc9e541161f71b61
costantinusa <costantinusa@gmail.com>
2026-08-19 21:38:25 +0200

    Actualizar caché PWA para módulo documentos

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='rmc-gestion-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='rmc-gestion-v3';
+const ASSETS=['./','./index.html','./documentos.html','./cuenta.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
